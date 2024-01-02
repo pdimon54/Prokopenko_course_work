5c96409743b296c10126cfd8535dee7f5c879b28
pdimon54 <pdimon54@gmail.com>
2024-01-02 20:55:20 +0200

    Initial commit

---
+++ src/main/java/org/example/IndexThreadsManager.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Set;
+
+public class IndexWorker {
+  private CustomThreadSafeHashMap<String, Set<String>> indexMap = new CustomThreadSafeHashMap<>();
+
+
+
+}
